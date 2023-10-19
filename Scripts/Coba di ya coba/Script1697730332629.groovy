823dd1ea0f128571b882d8b5b4edd9c84f813640
MacOs 01 <macos01@MacBeh.local>
2023-10-19 22:50:45 +0700

    testing dari mac 19102023

--- Scripts/Coba di ya coba/Script1697730332629.groovy
+++ Scripts/Coba di ya coba/Script1697730332629.groovy
@@ -9,17 +9,19 @@ import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
 import com.kms.katalon.core.model.FailureHandling as FailureHandling
 import com.kms.katalon.core.testcase.TestCase as TestCase
 import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
 import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
 
-WebUI.openBrowser('http://149.129.242.93/ikonhrm/symfony/web/index.php/auth/login')
+WebUI.openBrowser('www.google.com')
 
-WebUI.click(findTestObject(null))
+WebUI.maximizeWindow(FailureHandling.STOP_ON_FAILURE)
 
-WebUI.click(findTestObject(null))
+WebUI.delay(3)
 
 WebUI.closeBrowser(FailureHandling.STOP_ON_FAILURE)
 
